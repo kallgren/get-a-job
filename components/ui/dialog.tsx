2408b22191d34c3a50bb5c1242f3792d129b5932
Robert Källgren <robertkallgren@gmail.com>
2026-01-01 17:27:28 +0100

    Make dialog heading larger + add border

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -81,7 +81,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="dialog-header"
-      className={cn("flex flex-col gap-2 text-center sm:text-left", className)}
+      className={cn("flex flex-col gap-2 text-center sm:text-left border-b border-border pb-4", className)}
       {...props}
     />
   );
@@ -107,7 +107,7 @@ function DialogTitle({
   return (
     <DialogPrimitive.Title
       data-slot="dialog-title"
-      className={cn("text-lg leading-none font-semibold", className)}
+      className={cn("text-2xl leading-none font-semibold", className)}
       {...props}
     />
   );
